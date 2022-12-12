42b34b403bf9d30695db26f6a4ffa7b2a75d0ce7
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2022-12-12 11:47:38 +0200

    view toggle for further blog info

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,39 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+
+const Blog = ({ blog }) => {
+  const [viewAll, setViewAll] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const displayToggle = { display: viewAll ? '' : 'none' }
+
+  const toggleAll = () => {
+    setViewAll(!viewAll)
+    console.log(viewAll)
+  }
+
+  return (
+    <div style={blogStyle} >
+      <div>
+        {blog.title} - {blog.author}
+        <button onClick={toggleAll}>view</button>
+        <div style={displayToggle}>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes}</p>
+          <button>like</button>
+          <p>{blog.user.name}</p>
+        </div>
+      </div>
+    </div >
+  )
+
+}
 
 export default Blog
