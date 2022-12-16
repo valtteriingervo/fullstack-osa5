5d2c9b20a94ee9105a5ff92fa9fa53712582f8a9
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2022-12-16 10:43:57 +0200

    5.18

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -2,10 +2,18 @@ describe('Blog', function () {
   beforeEach(function () {
     // Reset the DB before each test
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    // Create user to the DB
+    const user = {
+      name: 'Valtteri Ingervo',
+      username: 'valttering',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     // And go to the site under test before each test
     cy.visit('http://localhost:3000')
   })
 
+  // t 5.17
   it('Login form is shown', function () {
     // Text expected is shown
     cy.contains('login')
@@ -15,5 +23,30 @@ describe('Blog', function () {
     cy.get('#username').type('typeTest')
     cy.get('#password').type('pwtest')
     cy.get('#login-button').click()
+  })
+
+  // t 5.18
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('valttering')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('valttering logged in')
+      cy.contains('blogs')
+      cy.contains('create new')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('valttering')
+      cy.get('#password').type('incorrect')
+      cy.get('#login-button').click()
+
+      cy.get('#notification')
+        .should('contain', 'Wrong username or password. Please try again')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.get('html').should('not.contain', 'valttering logged in')
+    })
   })
 })
--- src/components/Notification.js
+++ src/components/Notification.js
@@ -6,7 +6,7 @@ const Notification = ({ message, typeOfMessage }) => {
 
   return (
     <>
-      <div className={typeOfMessage}>
+      <div id='notification' className={typeOfMessage}>
         {message}
       </div>
     </>
